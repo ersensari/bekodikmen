7ecb004f3bb8d45a92f3010c373e7ecedf642c32
Ersen SARI <ersensari@gmail.com>
2020-12-23 01:22:23 +0300

    tanıtım afişi eklendi

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './semsiyelerim.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="Şemsiyelerim" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
